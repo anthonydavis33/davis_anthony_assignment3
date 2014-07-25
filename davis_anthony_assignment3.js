9a73f649eaf9507c6793ceb3524f247b2d56946f
anthonydavis33 <anthony.davis33@gmail.com>
2014-07-24 23:13:58 -0400

    finished up
    should've commited one more time before this...

--- davis_anthony_assignment3.js
+++ davis_anthony_assignment3.js
@@ -7,8 +7,9 @@
 // Global Variables
 var myName = "John",
     hasLice = true,
-    cleaningCert = false,
-    dragonName = "none"
+    napsTaken = 1,
+    dragonName = "none",
+    newDragonName = "none"
 ;
 
 var dragonNames = [
@@ -24,14 +25,17 @@ var dragonNames = [
 
 // Boolean Function
 var cleaningFunction = function (hasLice, myName) {
-    var delousing;
-    var cnt = 1;
 
     if (hasLice === true) {
         console.log("You may not enter into the daycare unless you have deloused him/her properly.");
     };
     while (hasLice === true) {
-        delousing = confirm("Have you properly deloused " + dragonName + " yet?");
+        var delousing = confirm("Have you properly deloused " + dragonName + " yet?");
+        while (hasLice === true) {
+            var specialShampoo = confirm("Did you use the special anti-dragonlice shampoo?");
+            console.log("You must use the John Johnson approved anti-dragonlice shampoo only!");
+        }
+        console.log("You may not enter into the daycare unless you have deloused him/her properly.");
         hasLice = !delousing;
     };
 
@@ -40,29 +44,39 @@ var cleaningFunction = function (hasLice, myName) {
 };
 
 // Array Function
-var dragonArray = function (dragonNames){
+var arrayFunction = function (){
+    
+    for (var key in jsonData.dragonNames) {
+        
+        for (var newKey in jsonData.dragonNames[key]) {
+            
+            console.log(key + ":" + newKey + ": " + jsonData.dragonNames[key][newKey]);
+        }
+    };
     
 };
 
 // Number Function
-var cleaningFunction = function (cleaningBool, myName) {
-    var dragonsWashed = 11;
-    var cnt = 1;
+var numberFunction = function (napsTaken, myName) {
+    var cnt = 7;
     
-    if (myName === "John") {
-        console.log("You've already taken taken the course, right " + myName);
-    };
-    while (cnt < dragonsWashed && myName !== "John") {
-        console.log("You have cleaned " + cnt + " Dragon(s).");
-        cnt ++;
-    };
+    console.log("Here at Dragon Daycare we take 6 naps throughout the day.");
+    
+    while (napsTaken < cnt) {
+        console.log(dragonName + " has napped " + napsTaken + " time(s).");
+        napsTaken++;
 
-    console.log(myName + " Has completed Dragon Cleaning!");
-    
+    };
+    napsTaken = napsTaken - 1;
+    return napsTaken;
 };
 
 // String Function
-
+var nicknameFunction = function(dragonName) {
+    var nicknamePrompt = prompt("What is a nickname for you?", "i.e. firebreather or humanslayer");
+    console.log(dragonName + ' "' + nicknamePrompt + '"' + " sounds terrifying! Great thinking!");
+    return nicknamePrompt;
+};
 
 console.log("My name is Miss Johns of Johnsville, Lead Dragon Daycare Specialist.");
 
@@ -78,6 +92,22 @@ if (myName === "John") {
 // Naming your dragon
 dragonName = prompt("What is the name of your Dragon?", "Think of a fun one!");
 
-cleaningFunction(cleaningBool, myName);
-console.log("--------------");
-console.log(cleaningBool);
+hasLice = confirm("Does your dragon have lice?");
+
+hasLice = cleaningFunction(hasLice, myName);
+
+napstaken = numberFunction(napsTaken, myName);
+
+console.log(dragonName + " is a good name, but lets add something fun");
+newDragonName = nicknameFunction(dragonName);
+
+console.log("Let's look at the roster of the top Dragons here");
+arrayFunction();
+console.log("Maybe one day you can be on this list " + dragonName + " " + newDragonName + ".");
+
+
+//Final Output
+
+console.log("With no lice found, " + napsTaken + " naps taken, " + dragonName + 
+        " was added the the nickname " + newDragonName);
+console.log("This was a fine day in Dragon Daycare.");
---
+++ jsonData.js
@@ -0,0 +1,24 @@
+// JSON Data
+
+var jsonData = {
+    
+    "dragonNames":
+            [
+                {
+                    "name": "Puff",
+                    "nickname": "Magic Dragon"
+                },
+                {
+                    "name": "Draco",
+                    "nickname": "Dark Knight"
+                },
+                {
+                    "name": "Igneel",
+                    "nickname": "of Warmth"
+                },
+                {
+                    "name": "Falkor",
+                    "nickname": "Defeats the Nothing"
+                }
+            ]
+};
